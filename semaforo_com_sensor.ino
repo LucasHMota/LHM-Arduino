c89646de78cec9d845c30b9dffe5907241a28cd1
LucasHM <58492603+LucasHMota@users.noreply.github.com>
2021-05-25 22:23:18 -0300

    Add files via upload

--- semaforo_com_sensor.ino
+++ semaforo_com_sensor.ino
@@ -1,9 +1,9 @@
 /*///////////////////////////////////////////////////////////////
 PROJETO ARDUINO ADS 4 2019 FATEC CRUZEIRO - PROF. WALDOMIRO MAY
 
-SEMÁFORO COM SENSORES DE APROXIMAÇÃO
+SEMÁFORO COM SENSORES DE APROXIMAÇÃO E CANCELA DE SEGURANÇA
 
-AUTOR: LUCAS MOTA
+AUTORES: LUCAS MOTA; MATHEUS; JOÃO VITOR; MARCOS; THIAGO
 //////////////////////////////////////////////////////////////*/
 
 
@@ -31,6 +31,18 @@ Ultrasonic sensor2(sensor2_trigger, sensor2_echo);
 int car1R = 7; //Vermelho
 int car1Y = 8; //Amarelo
 int car1G = 9; //Verde
+
+int car2R = 11; //Vermelho
+int car2Y = 12; //Amarelo
+int car2G = 13; //Verde
+//////////////////////////////////////////////////
+
+//Definir variáveis e posições dos semáforos de pedestres
+int ped1G = 30; //Verde
+int ped1R = 32; //Vermelho
+
+int ped2G = 48; //Verde
+int ped2R = 50; //Vermelho
 //////////////////////////////////////////////////
 
 //Definir variáveis e posições do buzzer
@@ -39,14 +51,26 @@ int frequencia = 0;
 int buzzer = 6;
 //////////////////////////////////////////////////
 
+//Definir variável do contador
+int contador;
+//////////////////////////////////////////////////
+
+
 void setup()
 {
   Serial.begin(9600);//padrão de saída de energia do Arduino
   
-  //Pino dos leds do semáforo
   pinMode (car1R, OUTPUT);
   pinMode (car1Y, OUTPUT);
   pinMode (car1G, OUTPUT);
+  pinMode (car2R, OUTPUT);
+  pinMode (car2Y, OUTPUT);
+  pinMode (car2G, OUTPUT);
+
+  pinMode (ped1G, OUTPUT);
+  pinMode (ped1R, OUTPUT);
+  pinMode (ped2G, OUTPUT);
+  pinMode (ped2R, OUTPUT);
 
   pinMode(buzzer,OUTPUT); //Pino do buzzer
   // Declaração de função, dizendo que os pinos digitais serão uma saída
@@ -60,22 +84,31 @@ void loop()
   cmMsec = sensor1.convert(microsec, Ultrasonic::CM);
   cmMsec2 = sensor2.convert(microsec2, Ultrasonic::CM);
 
-  //Programa inicia com o semáforo verde
   digitalWrite(car1G,HIGH);
-
-  //Funcionário ou pedestre pisa na plataforma de travessia
+  digitalWrite(car2G,HIGH);
+  digitalWrite(ped1R, HIGH);
+  digitalWrite(ped2R, HIGH);
+  
   if(cmMsec<5 || cmMsec2<5)
   {
     delay(5000);  
     digitalWrite (car1G, LOW); // LED Verde será desligado.
+    digitalWrite (car2G, LOW); // LED Verde será desligado.
     digitalWrite (car1Y, HIGH); // LED Amarelo será ligado.
+    digitalWrite (car2Y, HIGH); // LED Amarelo será ligado.
     
     delay (1000); // Espera um segundo.
     
     digitalWrite (car1Y, LOW); // LED Amarelo será desligado.
+    digitalWrite (car2Y, LOW); // LED Amarelo será desligado.
     digitalWrite (car1R, HIGH); // LED Vermelho será ligado.
+    digitalWrite (car2R, HIGH); // LED Vermelho será ligado.
+    
+    digitalWrite (ped1G, HIGH); // LED Verde do pedestre será ligado.
+    digitalWrite (ped2G, HIGH); // LED Verde do pedestre será ligado.
+    digitalWrite (ped1R, LOW); // LED Vermelho do pedestre será desligado.
+    digitalWrite (ped2R, LOW); // LED Vermelho do pedestre será desligado.
 
-    //É tocada a buzina
     for (frequencia = 150; frequencia < 1800; frequencia += 1) 
       {
         tone(buzzer, frequencia, tempo); 
@@ -84,7 +117,6 @@ void loop()
     
     delay(8000); // Espera 8 segundos
 
-    //Contagem regressiva para retorno do sinal verde    
     for (frequencia=0; frequencia<4; frequencia++) 
       {
         tone(6,262,500); //DO
@@ -92,6 +124,12 @@ void loop()
       }
       tone(6,262,1200); //DO
 
-    digitalWrite (car1R, LOW); // LED Vermelho será desligado e o ciclo retorna.
+    digitalWrite (car1R, LOW); // LED Vermelho será desligado.
+    digitalWrite (car2R, LOW); // LED Vermelho será desligado.
+    digitalWrite (ped1G, LOW); // LED Verde será desligado.
+    digitalWrite (ped2G, LOW); // LED Verde será desligado.
+
+    contador++;
+    Serial.println(contador);
   }
 }
